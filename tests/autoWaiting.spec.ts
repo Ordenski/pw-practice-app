4c4e907ba811ccf7c5fdf1bfd4cbc025cea951a2
Marko Ordenski <ordenski@M-HCJQN0WN7T.local>
2026-03-01 20:12:22 +0100

    auto waiting test fix

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -3,20 +3,11 @@ import {test, expect} from '@playwright/test'
 test.beforeEach(async({page}, testInfo) => {
     await page.goto('http://uitestingplayground.com/ajax')
     await page.getByText('Button Triggering AJAX Request').click()
-    testInfo.setTimeout(testInfo.timeout + 2000)
+    testInfo.setTimeout(testInfo.timeout + 12000)
 })
 
 test('auto waiting', async({page}) => {
     const successButton = page.locator('.bg-success')
-
-    //await successButton.click()
-
-    //const text = await successButton.textContent()
-
-    // await successButton.waitFor({state: "attached"})
-    // const text = await successButton.allTextContents()
-
-   //expect(text).toContain('Data loaded with AJAX get request.')
 
     await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
 })
@@ -28,10 +19,10 @@ test('alternative waits', async ({page}) => {
     // await page.waitForSelector('.bg-success')
 
     // wait for particular response
-    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+    await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
 
     // wait for network calls to be completed (NOT RECOMMENDED)
-    await page.waitForLoadState('networkidle')
+    //await page.waitForLoadState('networkidle')
 
     const text = await successButton.allTextContents()
     expect(text).toContain('Data loaded with AJAX get request.')
